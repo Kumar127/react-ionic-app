af9b54fa0749d77b7375521ba14d76e9f75c5282
Kumar127 <60001250+Kumar127@users.noreply.github.com>
2020-02-11 00:08:35 +0530

    2nd

---
+++ App.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { apps, flash, send, home, help} from 'ionicons/icons';
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
+import Details from './pages/Details';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/home" component={Tab1} exact={true} />
+          <Route path="/about" component={Tab2} exact={true} />
+          <Route path="/about/details" component={Details} />
+          <Route path="/setting" component={Tab3} />
+          <Route path="/" render={() => <Redirect to="/home" />} exact={true} />
+        </IonRouterOutlet>
+        <IonTabBar slot="top">
+          <IonTabButton tab="home" href="/home">
+            <IonIcon icon={home} />
+            <IonLabel>Home</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="about" href="/about">
+            <IonIcon icon={help} />
+            <IonLabel>Help</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="setting" href="/setting">
+            <IonIcon icon={send} />
+            <IonLabel>My Bookings</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
