fc63ef005daffab5693c23ab265e22f6e4e499e5
Kumar127 <60001250+Kumar127@users.noreply.github.com>
2020-02-13 23:56:24 +0530

    data

---
+++ Data.js
@@ -0,0 +1,123 @@
+ export  const Salon=[{ 
+     "Green Trends":{
+         "Step-Cutting":{
+             "price":120,
+             "time":20
+         },
+         "Twist":{
+            "price":120,
+            "time":20,
+            "desc":`A few sexy twists are a great way to breathe some life into your cropped hairstyle. Not only is this a unique look, but it will also protect your locks too. The twists require zero additional styling and will let your hair repair itself from damage sustained from heat, colouring and just the general wears and tears of life`
+        },
+        "Short wavey Bob":{
+            "price":320,
+            "time":60,
+            "desc":`The bob is one of the most significant hairstyle trends of late. 
+            It is the perfect combination of edgy yet pretty with just the right amount of masculinity to make it on trend.
+            If you’re tired of the classic bob, try mixing thing up with this wavy style. 
+            Bobs and waves are a match made in hair heaven. 
+            Nothing says cool-girl chic like a touselled just rolled out of bed crop.`
+        },
+        "Low Bun":{
+            "price":120,
+            "time":20,
+            "desc":`Even with the chicest cut, there will still be some days when you can’t resist the messy bun. It is the bad boy of hairstyles, we know we shouldn’t do it, but its allure is just too strong. With short hair, you may find yourself struggling to achieve the messy high bun. If so, go for the low option.  This may not be something you have thought of, but a low messy bun achieves the same level of simple styling as a high topknot while making your locks appear thicker too`
+        },
+        "Pixie cut":{
+            "price":120,
+            "time":20,
+            "desc":`Going short can be very daunting for women with thick hair. When it goes wrong, it goes very very wrong (we’ve all had those awkward bowl looking haircuts). But the key to nailing short styles with thick hair is layers and texture. Avoid blunt cuts as this style will add bulk to your hair. Instead go for a layered, textured pixie cut. The layers will take some the weight out of your luscious locks allowing them to sit and flow with ease.`
+        },
+        "Blunt Cut":{
+            "price":220,
+            "time":40,
+            "desc":`If you suffer from thin hair syndrome, you’ll know the struggle of wanting a cute pixie cut and leaving the salon looking like a baby-haired Draco Malfoy. Well, those dreaded days are over. A blunt cut is perfect for women with thin hair who still want to rock a shorter style. Not only are blunt cuts a great way to frame your face and accentuate your features, but they are also universally flattering, and very on trend.`
+        },
+        "Blunt Cut":{
+            "price":220,
+            "time":40,
+            "desc":`Bangs are back, and short hair is here to stay, so why not combine two of the biggest hair trends from recent years? Simple and easy to style, bangs also require very little maintenance, other than the occasional trim. Bangs in short hair work particularly well as the frame your face, drawing attention to your features. This can also be a slimming style if you find your short hair is making you look a little fuller in the face.`
+        },
+     },
+     "Naturals":{
+        "Step-Cutting":{
+            "price":120,
+            "time":20
+        },
+        "Twist":{
+           "price":120,
+           "time":20,
+           "desc":`A few sexy twists are a great way to breathe some life into your cropped hairstyle. Not only is this a unique look, but it will also protect your locks too. The twists require zero additional styling and will let your hair repair itself from damage sustained from heat, colouring and just the general wears and tears of life`
+       },
+       "Short wavey Bob":{
+           "price":320,
+           "time":60,
+           "desc":`The bob is one of the most significant hairstyle trends of late. 
+           It is the perfect combination of edgy yet pretty with just the right amount of masculinity to make it on trend.
+           If you’re tired of the classic bob, try mixing thing up with this wavy style. 
+           Bobs and waves are a match made in hair heaven. 
+           Nothing says cool-girl chic like a touselled just rolled out of bed crop.`
+       },
+       "Low Bun":{
+           "price":120,
+           "time":20,
+           "desc":`Even with the chicest cut, there will still be some days when you can’t resist the messy bun. It is the bad boy of hairstyles, we know we shouldn’t do it, but its allure is just too strong. With short hair, you may find yourself struggling to achieve the messy high bun. If so, go for the low option.  This may not be something you have thought of, but a low messy bun achieves the same level of simple styling as a high topknot while making your locks appear thicker too`
+       },
+       "Pixie cut":{
+           "price":120,
+           "time":20,
+           "desc":`Going short can be very daunting for women with thick hair. When it goes wrong, it goes very very wrong (we’ve all had those awkward bowl looking haircuts). But the key to nailing short styles with thick hair is layers and texture. Avoid blunt cuts as this style will add bulk to your hair. Instead go for a layered, textured pixie cut. The layers will take some the weight out of your luscious locks allowing them to sit and flow with ease.`
+       },
+       "Blunt Cut":{
+           "price":220,
+           "time":40,
+           "desc":`If you suffer from thin hair syndrome, you’ll know the struggle of wanting a cute pixie cut and leaving the salon looking like a baby-haired Draco Malfoy. Well, those dreaded days are over. A blunt cut is perfect for women with thin hair who still want to rock a shorter style. Not only are blunt cuts a great way to frame your face and accentuate your features, but they are also universally flattering, and very on trend.`
+       },
+       "Blunt Cut":{
+           "price":220,
+           "time":40,
+           "desc":`Bangs are back, and short hair is here to stay, so why not combine two of the biggest hair trends from recent years? Simple and easy to style, bangs also require very little maintenance, other than the occasional trim. Bangs in short hair work particularly well as the frame your face, drawing attention to your features. This can also be a slimming style if you find your short hair is making you look a little fuller in the face.`
+       },
+    },
+    "Lorial Paris":{
+        "Step-Cutting":{
+            "price":120,
+            "time":20
+        },
+        "Twist":{
+           "price":120,
+           "time":20,
+           "desc":`A few sexy twists are a great way to breathe some life into your cropped hairstyle. Not only is this a unique look, but it will also protect your locks too. The twists require zero additional styling and will let your hair repair itself from damage sustained from heat, colouring and just the general wears and tears of life`
+       },
+       "Short wavey Bob":{
+           "price":320,
+           "time":60,
+           "desc":`The bob is one of the most significant hairstyle trends of late. 
+           It is the perfect combination of edgy yet pretty with just the right amount of masculinity to make it on trend.
+           If you’re tired of the classic bob, try mixing thing up with this wavy style. 
+           Bobs and waves are a match made in hair heaven. 
+           Nothing says cool-girl chic like a touselled just rolled out of bed crop.`
+       },
+       "Low Bun":{
+           "price":120,
+           "time":20,
+           "desc":`Even with the chicest cut, there will still be some days when you can’t resist the messy bun. It is the bad boy of hairstyles, we know we shouldn’t do it, but its allure is just too strong. With short hair, you may find yourself struggling to achieve the messy high bun. If so, go for the low option.  This may not be something you have thought of, but a low messy bun achieves the same level of simple styling as a high topknot while making your locks appear thicker too`
+       },
+       "Pixie cut":{
+           "price":120,
+           "time":20,
+           "desc":`Going short can be very daunting for women with thick hair. When it goes wrong, it goes very very wrong (we’ve all had those awkward bowl looking haircuts). But the key to nailing short styles with thick hair is layers and texture. Avoid blunt cuts as this style will add bulk to your hair. Instead go for a layered, textured pixie cut. The layers will take some the weight out of your luscious locks allowing them to sit and flow with ease.`
+       },
+       "Blunt Cut":{
+           "price":220,
+           "time":40,
+           "desc":`If you suffer from thin hair syndrome, you’ll know the struggle of wanting a cute pixie cut and leaving the salon looking like a baby-haired Draco Malfoy. Well, those dreaded days are over. A blunt cut is perfect for women with thin hair who still want to rock a shorter style. Not only are blunt cuts a great way to frame your face and accentuate your features, but they are also universally flattering, and very on trend.`
+       },
+       "Blunt Cut":{
+           "price":220,
+           "time":40,
+           "desc":`Bangs are back, and short hair is here to stay, so why not combine two of the biggest hair trends from recent years? Simple and easy to style, bangs also require very little maintenance, other than the occasional trim. Bangs in short hair work particularly well as the frame your face, drawing attention to your features. This can also be a slimming style if you find your short hair is making you look a little fuller in the face.`
+       },
+    },
+
+}] 
