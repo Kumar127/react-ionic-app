9ccc4e9fcc1e0038dba34a79bdcd88c1b1df3916
Kumar127 <60001250+Kumar127@users.noreply.github.com>
2020-02-16 22:49:35 +0530

    Update Book.js

--- Book.js
+++ Book.js
@@ -1,115 +1,158 @@
-import React,{useState}from 'react';
-import {flash} from 'ionicons/icons';
-import hair from '../Assets/hair.jpg'
-import makeUp from '../Assets/make_up.jpg'
-import nails from '../Assets/nails.jpg'
+import React from 'react';
+import {menu, arrowForward, navigate} from 'ionicons/icons';
+import women_salon from '../Assests/hair.jpg'
+import AC from '../Assests/Ac_repair_pics.jpg'
+import signIn from '../Componenets/signUp/SIgnIn/SignIn'
+import men_salon from '../Assests/men_salon.jpg'
+import electricians from '../Assests/electricians.jpg'
+import plumber from '../Assests/plumber.jpg'
+import carpenters from '../Assests/carpentry.jpg'
+import clean from '../Assests/cleaning.png'
+import pest from '../Assests/Pest.jpg'
+import './Tab1.css'
 import {IonCard,
     IonContent,
     IonHeader,
     IonItem,
     IonLabel,
     IonPage,
-    IonToolbar, IonImg, IonButtons, IonText, IonList, IonIcon, IonCardHeader, IonCardTitle } from '@ionic/react';
-import './Book.css'
+    IonToolbar, IonButtons, IonText, IonList, IonIcon, IonCardHeader, IonCardTitle, IonAvatar, IonSlides, IonSlide, IonListHeader, IonItemSliding, IonItemOptions, IonItemOption, IonSearchbar } from '@ionic/react';
 
+import Sig from '../Componenets/signUp/SIgnIn/SignIn'
 function Book() {
-    const [showModal, setShowModal] = useState(false)
+    // const [showModal, setShowModal] = useState(false)
+    const slidesOptions:any = {
+      slidesPerView:1
+}
     
   return (
-    <IonPage>
-    <IonHeader >
-    <IonToolbar className="ion-toolbar">
+    <>
+    <IonHeader>
+    <IonToolbar className="ion-toolbar" color="tertiary">
       <IonButtons slot="start">
-        <IonIcon icon={flash} size="large" className="ion-icon-header" />
+        <IonIcon icon={navigate} size="large" className="ion-icon-header" slot="start"onClick={()=> window.location.href='/register'} />
         <IonText className="ion-text-header">GLAMSQUAD</IonText>
       </IonButtons>
     </IonToolbar>
   </IonHeader>
-    <IonContent className="ion-content">
+    <IonContent className="ion-content ion-padding">
+    <IonSearchbar placeholder="What are You Looking for" color="success"></IonSearchbar>
     <IonList className="ion-card">  
-      <IonCard className="ion-card">
-        <IonImg src={hair} className="ion-image"/>
+      <IonItem className="ion-card" >
+        <IonAvatar>
+          <img src={women_salon}/>
+        </IonAvatar>
           <IonCardHeader>
-            <IonCardTitle className="ion-card-title-img">HAIR</IonCardTitle>
+            <IonCardTitle className="ion-card-title-img">Ladies Salon</IonCardTitle>
           </IonCardHeader>
-      </IonCard>
-      <IonCard className="ion-card">
-        <IonImg src={makeUp} className="ion-image2"/>
+      </IonItem>
+      <IonItem className="ion-card" >
+        <IonAvatar>
+          <img src={AC}/>
+        </IonAvatar>
           <IonCardHeader>
-            <IonCardTitle className="ion-card-title-img">MAKE UP</IonCardTitle>
+            <IonCardTitle className="ion-card-title-img">AC Services</IonCardTitle>
           </IonCardHeader>
-      </IonCard>
-      <IonCard className="ion-card">
-        <IonImg src={nails} className="ion-image2"/>
+      </IonItem>
+      <IonItem className="ion-card" >
+        <IonAvatar>
+          <img src={men_salon}/>
+        </IonAvatar>
           <IonCardHeader>
-            <IonCardTitle className="ion-card-title-img">NAILS</IonCardTitle>
+            <IonCardTitle className="ion-card-title-img">Men's Grooming</IonCardTitle>
           </IonCardHeader>
-      </IonCard>
-            
+      </IonItem>
+      <IonItem className="ion-card" >
+        <IonAvatar>
+          <img src={electricians}/>
+        </IonAvatar>
+          <IonCardHeader>
+            <IonCardTitle className="ion-card-title-img">Electricians</IonCardTitle>
+          </IonCardHeader>
+      </IonItem>
+      <IonItem className="ion-card" >
+        <IonAvatar>
+          <img src={plumber} alt="nails" />
+        </IonAvatar>
+          <IonCardHeader>
+            <IonCardTitle className="ion-card-title-img">Plumber</IonCardTitle>
+          </IonCardHeader>
+      </IonItem>
+      <IonItem className="ion-card" >
+        <IonAvatar>
+          <img src={carpenters} alt="nails" />
+        </IonAvatar>
+          <IonCardHeader>
+            <IonCardTitle className="ion-card-title-img">carpenters</IonCardTitle>
+          </IonCardHeader>
+      </IonItem>
+      <IonItem className="ion-card" >
+        <IonAvatar>
+          <img src={clean} alt="nails"/>
+        </IonAvatar>
+          <IonCardHeader>
+            <IonCardTitle className="ion-card-title-img">Cleaning</IonCardTitle>
+          </IonCardHeader>
+      </IonItem>
+      <IonItem className="ion-card" >
+        <IonAvatar>
+          <img src={pest} alt="nails"/>
+        </IonAvatar>
+          <IonCardHeader>
+            <IonCardTitle className="ion-card-title-img">Painting & Pest Control</IonCardTitle>
+          </IonCardHeader>
+      </IonItem>
     </IonList>
+    <IonItem>
+        <IonLabel slot="start">Special Offers</IonLabel>
+        <IonIcon icon={arrowForward} mode="ios" slot="end"/>
+      </IonItem>
+    <IonSlides options={slidesOptions}>
+      <IonSlide>
+        <IonCard className="girl_card poster_card"></IonCard>
+      </IonSlide>
+      <IonSlide>
+        <IonCard className="ac_repair_card poster_card"></IonCard>
+      </IonSlide>
+      <IonSlide>
+        <IonCard className="Cleaning__card poster_card"></IonCard>
+      </IonSlide>
+      <IonSlide>
+        <IonCard className="Painting_card poster_card"></IonCard>
+      </IonSlide>
+    </IonSlides>
+  
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-      {/* <IonCard color="tertiary" className="ion-card">
-        <IonCardContent className="ion-content">
+    {/* list with sliding list animation below  */}
+    {/* <IonList>
+      <IonItemSliding>
         <IonItem>
-              <IonLabel  class="ion-label" position="stacked">Loan Amount</IonLabel> 
+          <IonLabel>Item</IonLabel>
         </IonItem>
-        <IonItem className="ion-range">
-          <IonRange className="ion-range" min={0} max={50} step={10} snaps={true}   >
-          <IonLabel slot="start">0</IonLabel>
-        <IonLabel slot="end">100</IonLabel>
-          </IonRange>
-    </IonItem>
-        </IonCardContent>
-      </IonCard>
-      <IonCard className="ion-card">
-        <IonCardContent className="ion-content">
+        <IonItemOptions side="end">
+          <IonItemOption onClick={() => {}}>Unread</IonItemOption>
+        </IonItemOptions>
+      </IonItemSliding>
+
+      <IonItemSliding>
         <IonItem>
-              <IonLabel class="ion-label" position="stacked">Tenure</IonLabel>
-              <IonInput></IonInput>
+          <IonLabel>Item</IonLabel>
         </IonItem>
-        <IonItem className="ion-range">
-          <IonRange className="ion-range" min={0} max={30} step={3} snaps={true}   >
-          <IonLabel slot="start">0</IonLabel>
-        <IonLabel slot="end">30</IonLabel>
-          </IonRange>
-    </IonItem>
-        </IonCardContent>
-      </IonCard>
-      <IonCard className="ion-card">
-        <IonCardContent className="ion-content">
-        <IonItem>
-              <IonLabel class="ion-label" position="stacked">Interest Rate</IonLabel>
-              <IonInput></IonInput>
-        </IonItem>
-        <IonItem className="ion-range">
-          <IonRange className="ion-range" min={0} max={20} step={4} snaps={true}   >
-          <IonLabel slot="start">0</IonLabel>
-        <IonLabel slot="end">20</IonLabel>
-          </IonRange>
-        </IonItem>
-        </IonCardContent>
-      </IonCard> */}
+        <IonItemOptions side="end">
+          <IonItemOption onClick={() => {}}>Unread</IonItemOption>
+        </IonItemOptions>
+      </IonItemSliding>
+    </IonList> */}
+
+      
       {/* <IonModal isOpen={showModal}>
         <p>This is modal content</p>
         <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
       </IonModal>
       <IonButton className="ion-button-modal" expand="block" onClick={() => setShowModal(true)}>CALCULATE</IonButton> */}
     </IonContent>
-  </IonPage>
+  </>
   );
 }
 
