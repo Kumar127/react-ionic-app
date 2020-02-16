73fc120ec3449904d393dfa56e5fe7ae0055723c
Kumar127 <60001250+Kumar127@users.noreply.github.com>
2020-02-16 22:45:23 +0530

    Add files via upload

---
+++ firebaseConfig.ts
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCTHoKfuP5mOP1ZQswIHXTK2TZyFFhe8tI",
+  authDomain: "glamsquad-2ca26.firebaseapp.com",
+  databaseURL: "https://glamsquad-2ca26.firebaseio.com",
+  projectId: "glamsquad-2ca26",
+  storageBucket: "glamsquad-2ca26.appspot.com",
+  messagingSenderId: "59118451987",
+  appId: "1:59118451987:web:754838474b291d11e26ee6",
+  measurementId: "G-6C6M7VX2X7"
+}
+firebase.initializeApp(config)
